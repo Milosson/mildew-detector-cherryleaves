2d9fdf0c359975a59c4b8f5de3e687e5538686e7
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-12-01 15:42:34 +0000

    Added content to multipage.py

---
+++ app_pages/multipage.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+            page_title="Cherry Leaves Mildew Detector",
+            page_icon="🖥️")  
+# Class to generate multiple Streamlit pages using an object oriented approach
+class MultiPage:
+
+    def __init__(self, app_name) -> None:
+        self.pages = []
+        self.app_name = app_name
+
+        
+
+    def add_page(self, title, func) -> None:
+        self.pages.append({"title": title, "function": func})
+
+    def run(self):
+        st.title(self.app_name)
+        page = st.sidebar.radio('Menu', self.pages, format_func=lambda page: page['title'])
+        page['function']()
