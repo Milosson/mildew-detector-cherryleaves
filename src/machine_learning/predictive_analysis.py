79fdfa68ab48166fe80338c816422d1735c70b01
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-12-02 12:00:54 +0000

    Push to Heroku, change directory for loading model in pradictive_analysis.py

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -49,7 +49,7 @@ def load_model_and_predict(my_image, version):
     Load and perform ML prediction over live images
     """
 
-    model = load_model(f"outputs/{version}/cherry-leaves-model-h1")
+    model = load_model(f"outputs/{version}/cherry-leaves-model-h2")
 
     pred_proba = model.predict(my_image)[0, 0]
 
